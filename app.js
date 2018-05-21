3896fddaf07f9ec6025a4713fa0d1d039ccdf1fd
AVO757 <vo.c.alexander@gmail.com>
2018-05-21 19:26:25 +0100

    test 3

--- app.js
+++ app.js
@@ -71,6 +71,6 @@ app.use(contactRoute);
 
 
 /////////////////////////////////////////////////
-app.listen(3000 || process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("server started");
 });
